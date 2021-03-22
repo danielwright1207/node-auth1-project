874da3115399b49639d74b2174e1efcbfc8a7b31
Daniel Wright <daniel.wright1207@gmail.com>
2021-03-22 17:52:44 -0600

    test are broken this is the best i could

--- api/server.js
+++ api/server.js
@@ -1,6 +1,52 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const usersRouter = require('./users/users-router');
+const authRouter = require('./auth/auth-router');
+
+const server = express();
+
+server.use(session({
+  name: 'chocolatechip',
+  secret: 'keep it secret, keep it safe',
+  cookie: {
+    maxAge: 1000 * 10 * 1000,
+    secure: false,
+    httpOnly: false,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: require('../data/db-config'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+}))
+
+
+server.use('/api/users', usersRouter)
+server.use('/api/auth', authRouter)
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.json('hello there')
+});
+
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -14,22 +60,3 @@ const cors = require("cors");
   The session can be persisted in memory (would not be adecuate for production)
   or you can use a session store like `connect-session-knex`.
  */
-
-const server = express();
-
-server.use(helmet());
-server.use(express.json());
-server.use(cors());
-
-server.get("/", (req, res) => {
-  res.json({ api: "up" });
-});
-
-server.use((err, req, res, next) => { // eslint-disable-line
-  res.status(500).json({
-    message: err.message,
-    stack: err.stack,
-  });
-});
-
-module.exports = server;
